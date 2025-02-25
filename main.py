1da8e0db397c8719697163642a45b630c2d32a96
DeveloperTanmay <umas81261@gmail.com>
2025-02-25 19:02:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Operations on Dictionary
+
+dict = {1,2,2,3,3,4,4,4,5,5,6,7}
+print("Value of Dictionary : " , dict)
+
+dict.add(8)
+print("Value Of Dictionary After Adding 8 : " , dict)
+
+dict.remove(4)
+print("Value Of Dictionary After Removing 4 : " , dict)
+
+dict.clear()
+print("Value Of Dictionary After Clearing Dictionary : " , dict)
+
